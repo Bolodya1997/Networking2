9b5b869c71a0de3900da7f8737b48868df2371fa
bolodya <Vovan1_2@mail.ru>
2016-09-28 17:24:02 +0700

    ...

--- src/main/java/ru/nsu/fit/bolodya/networking/client/Client.java
+++ src/main/java/ru/nsu/fit/bolodya/networking/client/Client.java
@@ -9,10 +9,10 @@ public class Client {
         Socket socket = new Socket(host, port);
         byte message[] = new byte[1024 * 1024 * 200];
         while (true) {
-//            try {
-//                Thread.sleep(1000);
-//            }
-//            catch (InterruptedException ignored) {}
+            try {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException ignored) {}
             socket.getOutputStream().write(message);
         }
     }
